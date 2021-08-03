da816cc1e96605518988a444b4ec6002a13b03c2
matthewcroughan <matt@croughan.sh>
2021-08-03 22:03:09 +0100

    got it building to $out!

--- package.nix
+++ package.nix
@@ -1,6 +1,14 @@
 { mkYarnPackage }:
 
-mkYarnPackage {
+mkYarnPackage rec {
   src = ./.;
   pname = "kiwiirc";
+#  distPhase = "false";
+  doDist = false;
+  buildPhase = ''
+    yarn --offline run build
+    find . -type d -name dist
+    mkdir $out
+    mv ./deps/${pname}/dist/* $out/
+  '';
 }
