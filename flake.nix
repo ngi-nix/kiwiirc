095ab30c9fb3c39f977461a408c2b8469b46e0b1
matthewcroughan <matt@croughan.sh>
2021-08-06 16:55:00 +0100

    embed mkYarnPackage in Flake

--- flake.nix
+++ flake.nix
@@ -16,7 +16,21 @@
       nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; overlays = [ self.overlay yarn2nix.overlay ]; });
     in {
       overlay = final: prev: {
-        kiwiirc = final.callPackage ./package.nix { };
+        kiwiirc = final.mkYarnPackage rec {
+          src = ./.;
+          pname = "kiwiirc";
+          distPhase = "true";
+          buildPhase = ''
+            yarn --offline run build
+          '';
+          preInstall = ''
+            mkdir -p $out/www/${pname}
+            cp -r ./deps/${pname}/dist/* $out/www/${pname}
+          '';
+          postFixup = ''
+            rm -rf $out/tarballs $out/libexec $out/bin
+          '';
+        };
       };
 
       packages = forAllSystems (system:
